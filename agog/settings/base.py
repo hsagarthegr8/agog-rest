1c301bf114df42a45426b4f67d090784d5e3b1af
Himanshu Sagar <hsagarthegr8@gmail.com>
2018-09-23 18:19:37 +0530

    Added JWT authentication

--- agog/settings/base.py
+++ agog/settings/base.py
@@ -125,3 +125,15 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'accounts.User'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
--- api/accounts/urls.py
+++ api/accounts/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
+from rest_framework_jwt.views import obtain_jwt_token,refresh_jwt_token, verify_jwt_token
+
 from .views import UserList, VerificationList
 
 urlpatterns = [
     path('users', UserList.as_view()),
-    path('verifications', VerificationList.as_view())
+    path('verifications', VerificationList.as_view()),
+    path('get-token', obtain_jwt_token),
+    path('refresh-token', refresh_jwt_token),
+    path('verify-token', verify_jwt_token)
 ]
